101cefc406b59a726a3cbc62f6e5ce1c9592ea74
PetePete1984 <k_iller21@hotmail.com>
2018-01-03 20:57:53 +0100

    Minor cleanup in scripts

--- Scripts/Model/Planet.gd
+++ Scripts/Model/Planet.gd
@@ -42,10 +42,6 @@ var population = {
 
 var total_population = 0 setget ,get_total_population
 
-# current project
-# FIXME: remove this
-#var project = null
-
 # TODO: methods should probably be in a PlanetController, to keep data separate from interaction
 func get_total_population():
 	return population.work + population.idle + population.free
