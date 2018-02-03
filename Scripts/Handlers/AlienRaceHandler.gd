1866535880549fc1c797580d4e8cb51c087500b0
PetePete1984 <k_iller21@hotmail.com>
2018-02-03 13:54:55 +0100

    AlienRaceHandler starts research projects

--- Scripts/Handlers/AlienRaceHandler.gd
+++ Scripts/Handlers/AlienRaceHandler.gd
@@ -2,6 +2,7 @@
 extends Reference
 
 const ColonyManager = preload("res://Scripts/ColonyManager.gd")
+const ResearchManager = preload("res://Scripts/ResearchManager.gd")
 
 static func handle(player):
 	for c in player.colonies:
@@ -9,7 +10,17 @@ static func handle(player):
 		var build_next = ColonyManager.manage(colony)
 		if build_next != null:
 			if player.race.type == "fludentri":
-				print(build_next)
-				print(colony.planet.planet_name)
-				print(player.race.type)
-			ColonyManager.start_colony_project(colony, build_next.project, build_next.type, build_next.square)
+				printt(build_next, colony.planet.planet_name, player.race.type, GameStateHandler.game_state.turn)
+			if build_next.project != null:
+				ColonyManager.start_colony_project(colony, build_next.project, build_next.type, build_next.square)
+			else:
+				#print(build_next)
+				pass
+	# find available research project if none is running
+	# follow some research path
+	# pick research
+	# do black magic for cheaters, err, chamachies
+	if player.total_research > 0:
+		var research_next = ResearchManager.manage(player)
+		if research_next != null:
+			ResearchHandler.start_research(player, research_next)
