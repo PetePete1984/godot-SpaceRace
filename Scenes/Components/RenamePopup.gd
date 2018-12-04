a05522c2ee5eee97d354cfdc55e160adb58b2689
PetePete1984 <k_iller21@hotmail.com>
2018-12-04 12:55:30 +0100

    Update RenamePopup.gd

--- Scenes/Components/RenamePopup.gd
+++ Scenes/Components/RenamePopup.gd
@@ -6,12 +6,18 @@ onready var button = get_node("PanelContainer/VBoxContainer/HBoxContainer/Button
 
 signal popup_closed(result)
 
+func set_ship_size(size):
+	label.set_text("Name Your Ship")
+	line_edit.set_placeholder(size.capitalize())
+
 func set_ship(ship):
+	label.set_text("Name Your Ship")
 	line_edit.set_placeholder(ship.ship_name)
 
 func _ready():
 	button.connect("pressed", self, "_on_confirm_name", [null])
 	line_edit.connect("text_entered", self, "_on_confirm_name")
+	line_edit.grab_focus()
 
 func _on_confirm_name(text = null):
 	var new_name = ""
